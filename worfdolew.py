9cbb065a1b8ee86a5a3e456b1c59b89d96456d24
mugman <53713683+nature174@users.noreply.github.com>
2022-03-18 16:26:20 +0100

    User gets 6 chances instead of 5, don't repeat yellow or green when not needed

--- worfdolew.py
+++ worfdolew.py
@@ -6,35 +6,40 @@ RED, GREEN, YELLOW, GRAY, RESET = Back.RED, Back.GREEN, Back.YELLOW, Style.DIM, 
 print(f"{GREEN}Worfdolew - The Worfd Game\nEnter your guesses and see if they're correct. {RESET}\n\n")
 
 with open("wordlist.txt") as wordlist:
-    words = wordlist.read().strip().splitlines()
+	words = wordlist.read().strip().splitlines()
 word = random.choice(words)
 won = False
 
 def handle_guess(guess):
-    global words
-    if word not in words:
-        return f"{RED}{guess}{RESET}", False
-    if guess == word:
-        return f"{GREEN}{guess}{RESET}", True
-    guess = list(guess[:5])
-    for i in range(len(guess)):
-        if guess[i] == word[i]:
-            guess[i] = f"{GREEN}{guess[i]}{RESET}"
-        elif guess[i] in word:
-            guess[i] = f"{YELLOW}{guess[i]}{RESET}"
-    return ' '.join(guess), False
+	global words
+	already = []
+	if word not in words:
+		return f"{RED}{guess}{RESET}", False
+	if guess == word:
+		return f"{GREEN}{guess}{RESET}", True
+	guess = list(guess[:5])
+	for i in range(len(guess)):
+		if guess[i] == word[i] and guess[i] not in already:
+			already.append(guess[i])
+			guess[i] = f"{GREEN} {guess[i]} {RESET}"
+		elif guess[i] in word and guess[i] not in already:
+			already.append(guess[i])
+			guess[i] = f"{YELLOW} {guess[i]} {RESET}"
+		else:
+			guess[i] = f"{GRAY} {guess[i]} {RESET}"
+	return ' '.join(guess), False
 
-for i in range(5):
-    guess = input("\r")
-    print(f"\033[F", end="")
-    x = handle_guess(guess)
-    sys.stdout.flush()
-    if x[0]: print(x[0])
-    if x[1]:
-        won = True
-        break
+for i in range(6):
+	guess = input("\r")
+	print(f"\033[F", end="")
+	x = handle_guess(guess)
+	sys.stdout.flush()
+	if x[0]: print(x[0])
+	if x[1]:
+		won = True
+		break
 
 if won:
-    print("\nYou won!")
+	print("\nYou won!")
 else:
-    print(f"The word was {word}.")
+	print(f"The word was {word}.")
